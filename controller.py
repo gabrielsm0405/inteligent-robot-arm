e03b2a9cb773d326631c6eae78489944422cbd31
gabrielmarquesitemm <gabriel.marques@itemm.org.br>
2023-05-05 01:02:08 -0300

    Melhorias

--- controller.py
+++ controller.py
@@ -32,28 +32,52 @@ learning_model = load('learning_model3.joblib')
 vel=180
 accel=90
 
-def moveToConfig(handles,gripperHandle, cuboidHandle, precision):
-    curTarget = sim.simxGetObjectPosition(clientID, cuboidHandle, -1, sim.simx_opmode_blocking)[1]
+def moveToHandle(jointHandles, gripperHandle, targetHandle, precision):
+    curTarget = sim.simxGetObjectPosition(clientID, targetHandle, -1, sim.simx_opmode_blocking)[1]
     error = 999
     delta = 0.01
+
     while error > precision:
         anglesPred = learning_model.predict([curTarget])[0].tolist()
-        for i in range(len(handles)):
-            currentConf = sim.simxGetJointPosition(clientID, handles[i], sim.simx_opmode_blocking)[1]
+        for i in range(len(jointHandles)):
+            currentConf = sim.simxGetJointPosition(clientID, jointHandles[i], sim.simx_opmode_blocking)[1]
             diff = anglesPred[i]*(math.pi/180) - currentConf
-            if diff > 0:
-                sim.simxSetJointTargetPosition(clientID, handles[i], currentConf+delta, sim.simx_opmode_oneshot)
-            else:
-                sim.simxSetJointTargetPosition(clientID, handles[i], currentConf-delta, sim.simx_opmode_oneshot)
+            if diff > delta:
+                sim.simxSetJointTargetPosition(clientID, jointHandles[i], currentConf+delta, sim.simx_opmode_oneshot)
+            elif diff < delta:
+                sim.simxSetJointTargetPosition(clientID, jointHandles[i], currentConf-delta, sim.simx_opmode_oneshot)
         
         output = sim.simxGetObjectPosition(clientID, gripperHandle, -1, sim.simx_opmode_blocking)[1]
         pred_output = identification_model.predict([anglesPred])[0].tolist()
         feedback = np.subtract(output, pred_output).tolist()
-        reference = sim.simxGetObjectPosition(clientID, cuboidHandle, -1, sim.simx_opmode_blocking)[1]
+        reference = sim.simxGetObjectPosition(clientID, targetHandle, -1, sim.simx_opmode_blocking)[1]
 
         curTarget = np.subtract(reference, feedback).tolist()
 
-        error = np.linalg.norm(np.subtract(output, reference))   
+        error = np.linalg.norm(np.subtract(output, reference))
+
+def moveToPosition(jointHandles, gripperHandle, targetPos, precision):
+    curTarget = targetPos
+    error = 999
+    delta = 0.02
+    while error > precision:
+        anglesPred = learning_model.predict([curTarget])[0].tolist()
+        for i in range(len(jointHandles)):
+            currentConf = sim.simxGetJointPosition(clientID, jointHandles[i], sim.simx_opmode_blocking)[1]
+            diff = anglesPred[i]*(math.pi/180) - currentConf
+            if diff > 0:
+                sim.simxSetJointTargetPosition(clientID, jointHandles[i], currentConf+delta, sim.simx_opmode_oneshot)
+            else:
+                sim.simxSetJointTargetPosition(clientID, jointHandles[i], currentConf-delta, sim.simx_opmode_oneshot)
+        
+        output = sim.simxGetObjectPosition(clientID, gripperHandle, -1, sim.simx_opmode_blocking)[1]
+        pred_output = identification_model.predict([anglesPred])[0].tolist()
+        feedback = np.subtract(output, pred_output).tolist()
+        reference = targetPos
+
+        curTarget = np.subtract(reference, feedback).tolist()
+
+        error = np.linalg.norm(np.subtract(output, reference))
         
 
 print ('Program started')
@@ -68,28 +92,28 @@ if clientID!=-1:
     jointHandles[1] = sim.simxGetObjectHandle(clientID, "/joint/joint", sim.simx_opmode_blocking)[1]
     jointHandles[2] = sim.simxGetObjectHandle(clientID, "/joint/joint/joint", sim.simx_opmode_blocking)[1]
     jointHandles[3] = sim.simxGetObjectHandle(clientID, "/joint/joint/joint/joint", sim.simx_opmode_blocking)[1]
+
     gripperHandle = sim.simxGetObjectHandle(clientID, "/PhantomXPincher/gripperCenter_joint", sim.simx_opmode_blocking)[1]
-    gripperCloseHandle = sim.simxGetObjectHandle(clientID, "/PhantomXPincher/gripperClose_joint", sim.simx_opmode_blocking)[1]
+    
     cuboidHandle = sim.simxGetObjectHandle(clientID, "/Cuboid", sim.simx_opmode_blocking)[1]
-    cupHandle = sim.simxGetObjectHandle(clientID, "/Disc", sim.simx_opmode_blocking)[1]
+    discHandle = sim.simxGetObjectHandle(clientID, "/Disc", sim.simx_opmode_blocking)[1]
 
     sim.simxSetInt32Signal(clientID, 'PhantomXPincher__15___gripperClose', 0, sim.simx_opmode_blocking)
 
-    moveToConfig(
+    moveToHandle(
         jointHandles,
         gripperHandle,
         cuboidHandle,
-        0.03
+        0.027
     )
 
-    print("close gripper")
     sim.simxSetInt32Signal(clientID, 'PhantomXPincher__15___gripperClose', 1, sim.simx_opmode_blocking)
 
-    moveToConfig(
+    moveToPosition(
         jointHandles,
         gripperHandle,
-        cupHandle,
-        0.07
+        [-0.375, 0.075, 0.2],
+        0.09
     )
 
     sim.simxSetInt32Signal(clientID, 'PhantomXPincher__15___gripperClose', 0, sim.simx_opmode_blocking)
